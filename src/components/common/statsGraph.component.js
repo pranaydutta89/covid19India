f549e8c4cb6f46d59be2a021bba0236b8b88b58c
pranay dutta <pranaydutta89@gmail.com>
2020-04-23 21:02:09 +0530

    fix

--- src/components/common/statsGraph.component.js
+++ src/components/common/statsGraph.component.js
@@ -1,12 +1,21 @@
 import React, { PureComponent } from 'react';
 import { Typography, LinearProgress } from '@material-ui/core'
 
+const css = `
+.recovered-progress-color-primary{
+    background-color:#c5f6c5;
+}
+.recovered-progress-bar-color-primary{
+    background-color:#06d109;
+}
+`
 export default class StatsGraph extends PureComponent {
 
 
     render() {
         const { active, confirmed, deceased, recovered } = this.props;
         return (<>
+            <style>{css}</style>
             <Typography color="textSecondary" gutterBottom>
                 <strong>Active ({active})</strong>
                 <LinearProgress variant="determinate" value={(active / confirmed) * 100} />
@@ -19,7 +28,10 @@ export default class StatsGraph extends PureComponent {
 
             <Typography color="textSecondary" gutterBottom>
                 <strong>Recovered ({recovered})</strong>
-                <LinearProgress color="secondary" variant="determinate" value={(recovered / confirmed) * 100} />
+                <LinearProgress classes={{
+                    colorPrimary: 'recovered-progress-color-primary',
+                    barColorPrimary: 'recovered-progress-bar-color-primary'
+                }} variant="determinate" value={(recovered / confirmed) * 100} />
             </Typography>
         </>
         );
