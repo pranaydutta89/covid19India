5990fdfeaf908fab1ca5e370f50065f7354160f9
pranay dutta <pranaydutta89@gmail.com>
2020-04-26 14:03:10 +0530

    service worker removed

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,7 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
-const WorkboxPlugin = require('workbox-webpack-plugin');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 module.exports = {
     entry: './src/entry.js',
@@ -24,11 +23,7 @@ module.exports = {
     plugins: [
         new MomentLocalesPlugin(),
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({ template: './src/index.html' }),
-        new WorkboxPlugin.GenerateSW({
-            clientsClaim: true,
-            skipWaiting: true,
-        })
+        new HtmlWebpackPlugin({ template: './src/index.html' })
     ],
     optimization: {
         splitChunks: {
