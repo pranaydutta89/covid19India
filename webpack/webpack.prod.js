f6eb42ed0e0e2e92d15aecc163dfeb7fce2b27e6
pranay dutta <pranaydutta89@gmail.com>
2020-05-10 01:17:32 +0530

    config changes

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin;
+    .BundleAnalyzerPlugin;
 const webpack = require('webpack');
 const SitemapPlugin = require('sitemap-webpack-plugin').default;
 const RobotstxtPlugin = require('robotstxt-webpack-plugin');
@@ -9,19 +9,24 @@ const RobotstxtPlugin = require('robotstxt-webpack-plugin');
 const paths = ['/', '/states', '/watched', '/india', '/about'];
 
 module.exports = merge(common, {
-  mode: 'production',
-  plugins: [
-    new SitemapPlugin('https://covid2.in', paths, {
-      lastmod: true,
-      changefreq: 'daily',
-    }),
-    new RobotstxtPlugin({
-      sitemap: 'https://covid2.in/sitemap.xml',
-      host: 'https://covid2.in',
-    }),
-    new BundleAnalyzerPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"',
-    }),
-  ],
+    mode: 'production',
+    plugins: [
+        new SitemapPlugin('https://covid2.in', paths, {
+            lastmod: true,
+            changefreq: 'daily',
+        }),
+        new RobotstxtPlugin({
+            sitemap: 'https://covid2.in/sitemap.xml',
+            host: 'https://covid2.in',
+        }),
+        new BundleAnalyzerPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"',
+        }),
+    ],
+    optimization: {
+        usedExports: true,
+        noEmitOnErrors: true
+    }
+
 });
